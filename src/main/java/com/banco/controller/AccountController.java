1b3141b4b625051fe421100646bd5039812600cf
Clauberson <claubersonpinheiro@gmail.com>
2024-05-30 20:57:51 -0300

    Configurando documentação com Swagger

--- src/main/java/com/banco/controller/AccountController.java
+++ src/main/java/com/banco/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.banco.controller;
 
+import java.awt.print.Book;
 import java.math.BigDecimal;
 
 import org.slf4j.Logger;
@@ -7,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,27 +19,49 @@ import org.springframework.web.bind.annotation.RestController;
 import com.banco.model.Account;
 import com.banco.service.AccountService;
 
-@RestController	
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
 @RequestMapping("/accounts")
 public class AccountController {
 	private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
-	
-    @Autowired	
-    private AccountService accountService;
 
-    @PostMapping("/{customerId}")
-    public ResponseEntity<Account> createAccount(@PathVariable Long customerId, @RequestBody Account account) {
-    	logger.debug("Received request to create account for customer ID: {}", customerId);
-    	Account createdAccount = accountService.createAccount(customerId, account);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdAccount);
-    }
+	@Autowired
+	private AccountService accountService;
 
-    @PostMapping("/transfer")
-    public ResponseEntity<Void> transfer(@RequestParam Long fromAccountId, @RequestParam Long toAccountId, @RequestParam BigDecimal amount) {
-    	logger.debug("Received transfer request from account {} to account {} with amount {}", fromAccountId, toAccountId, amount);
-    	accountService.transfer(fromAccountId, toAccountId, amount);
-        return ResponseEntity.ok().build();
-    }
+	@Operation(summary = "Create account")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Account created with sucess", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Account.class)) }),
+			@ApiResponse(responseCode = "400", description = "Error creating account", content = @Content),
+			@ApiResponse(responseCode = "404", description = "Customer not found", content = @Content) })
+	@PostMapping("/{customerId}")
+	public ResponseEntity<Account> createAccount(@Parameter(description = "id of customer") @PathVariable Long customerId, 
+			@Parameter(description = "account object") @RequestBody Account account) {
+		logger.debug("Received request to create account for customer ID: {}", customerId);
+		Account createdAccount = accountService.createAccount(customerId, account);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdAccount);
+	}
 
-    // Outros endponits para as operaçoes CRUD
-}	
+	@Operation(summary = "Create transfer")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Transfer created with sucess", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Account.class)) }),
+			@ApiResponse(responseCode = "400", description = "Error creating transfer", content = @Content)})
+	@PostMapping("/transfer")
+	public ResponseEntity<Void> transfer(@RequestParam Long fromAccountId, 
+			@Parameter(description = "Target account") @RequestParam Long toAccountId,
+			@Parameter(description = "Origin account") @RequestParam BigDecimal amount) {
+		logger.debug("Received transfer request from account {} to account {} with amount {}", fromAccountId,
+				toAccountId, amount);
+		accountService.transfer(fromAccountId, toAccountId, amount);
+		return ResponseEntity.ok().build();
+	}
+
+	// Outros endponits para as operaçoes CRUD
+}
--- src/main/java/com/banco/controller/CustomerController.java
+++ src/main/java/com/banco/controller/CustomerController.java
@@ -10,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.banco.model.Account;
 import com.banco.model.Customer;
 import com.banco.service.CustomerService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/customers")
@@ -21,8 +29,13 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @Operation(summary = "Create customer")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Customer created with sucess", content = {
+					@Content(mediaType = "application/json", schema = @Schema(implementation = Customer.class)) }),
+			@ApiResponse(responseCode = "400", description = "Error creating customer", content = @Content) })
     @PostMapping
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+    public ResponseEntity<Customer> createCustomer(@Parameter(description = "Customer object") @RequestBody Customer customer) {
     	logger.debug("Received request to create customer: {}", customer.getDocument());
         Customer createdCustomer = customerService.createCustomer(customer);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
