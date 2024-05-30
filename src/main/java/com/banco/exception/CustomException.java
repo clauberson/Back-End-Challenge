6c86238bd77255a50259161affd810e0263da3d3
Clauberson <claubersonpinheiro@gmail.com>
2024-05-30 17:57:49 -0300

    Ajuste logs

--- src/main/java/com/banco/exception/CustomException.java
+++ src/main/java/com/banco/exception/CustomException.java
@@ -1,9 +1,15 @@
 package com.banco.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CustomException extends RuntimeException {
     private static final long serialVersionUID = 1L;
+    
+    private static final Logger logger = LoggerFactory.getLogger(CustomException.class);
 
 	public CustomException(String message) {
+		logger.error("CustomException: {}", message);
         super(message);
     }
 }
