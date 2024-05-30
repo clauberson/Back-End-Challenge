467bcb7d57c19f7e9413ea0f553b2165401b47da
Clauberson <claubersonpinheiro@gmail.com>
2024-05-30 12:52:45 -0300

    Criando entidades

--- src/main/java/com/banco/model/Account.java
+++ src/main/java/com/banco/model/Account.java
@@ -1,5 +1,29 @@
 package com.banco.model;
 
-public class Account {
+import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String agency;
+    private BigDecimal balance;
+    private Boolean active;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    
 }
--- src/main/java/com/banco/model/Customer.java
+++ src/main/java/com/banco/model/Customer.java
@@ -1,5 +1,22 @@
 package com.banco.model;
 
-public class Customer {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Entity
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String document;
+    private String address;
+    private String password;
+    
 }
