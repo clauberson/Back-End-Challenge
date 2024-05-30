178068f38a28cf926ed6421dea04f51a7b807a93
Clauberson <claubersonpinheiro@gmail.com>
2024-05-30 13:04:14 -0300

    Criação dos controllers

--- src/main/java/com/banco/controller/AccountController.java
+++ src/main/java/com/banco/controller/AccountController.java
@@ -1,5 +1,37 @@
 package com.banco.controller;
 
-public class AccountController {
+import java.math.BigDecimal;
 
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banco.model.Account;
+import com.banco.service.AccountService;
+
+@RestController	
+@RequestMapping("/accounts")
+public class AccountController {
+    @Autowired	
+    private AccountService accountService;
+
+    @PostMapping("/{customerId}")
+    public ResponseEntity<Account> createAccount(@PathVariable Long customerId, @RequestBody Account account) {
+        Account createdAccount = accountService.createAccount(customerId, account);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAccount);
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<Void> transfer(@RequestParam Long fromAccountId, @RequestParam Long toAccountId, @RequestParam BigDecimal amount) {
+        accountService.transfer(fromAccountId, toAccountId, amount);
+        return ResponseEntity.ok().build();
+    }
+
+    // Outros endponits para as operaçoes CRUD
+}	
--- src/main/java/com/banco/controller/CustomerController.java
+++ src/main/java/com/banco/controller/CustomerController.java
@@ -1,5 +1,27 @@
 package com.banco.controller;
 
-public class CustomerController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-}
+import com.banco.model.Customer;
+import com.banco.service.CustomerService;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer createdCustomer = customerService.createCustomer(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
+    }
+    
+    // Outros endponits para as operaçoes CRUD
+}	
